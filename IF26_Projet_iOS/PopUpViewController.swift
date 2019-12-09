999d8104964555beccaafe48a20e12d81059f3e8
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-12-09 11:35:36 +0100

    Layout problem fixed Add show/remove animate for popUpView

--- IF26_Projet_iOS/PopUpViewController.swift
+++ IF26_Projet_iOS/PopUpViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class PopUpViewController: UIViewController {
 
-    @IBAction func CloseOnClick(_ sender: UIButton) {
-        self.view.removeFromSuperview()
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        self.showAnimate()
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func CloseOnClick(_ sender: UIButton) {
+    //        self.view.removeFromSuperview()
+            self.removeAnimate()
+    }
+    
+    func showAnimate(){
+        self.view.transform = CGAffineTransform(scaleX: 1.3,y: 1.3)
+        self.view.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0,y: 1.0)
+        })
+        
+    }
+    
+    func removeAnimate(){
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3,y: 1.3)
+            self.view.alpha = 0.0
+        }, completion: {(finished: Bool) in
+            if(finished){
+                self.view.removeFromSuperview()
+            }
+        })
+    }
     /*
     // MARK: - Navigation
 
